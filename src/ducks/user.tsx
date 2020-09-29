45a8c257418e411299bce849ebe43ac5c4924cb2
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-09-29 17:34:11 -0300

    Adição do redux ao projeto

---
+++ src/ducks/user.tsx
@@ -0,0 +1,41 @@
+import { AnyAction } from 'redux';
+
+interface UserDuck {
+  login: string | null;
+  loadingLogin: boolean;
+}
+
+const INITIAL_STATE: UserDuck = {
+  login: null,
+  loadingLogin: false,
+};
+
+const INIT_LOGIN = 'user_init_login';
+const LOGIN_USER = 'user_login_user';
+
+export default (state = INITIAL_STATE, action: AnyAction): UserDuck => {
+  switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, login: action.payload, loadingLogin: false };
+    case INIT_LOGIN:
+      return { ...state, loadingLogin: true };
+    default:
+      return state;
+  }
+};
+
+export const submitLogin = ({
+  username,
+  callback,
+}: {
+  username: string;
+  password: string;
+  callback: () => void;
+}) => {
+  return async (dispatch: any) => {
+    dispatch({ type: INIT_LOGIN });
+    setTimeout(function () {
+      dispatch({ type: LOGIN_USER, payload: username }, callback());
+    }, 5000);
+  };
+};
